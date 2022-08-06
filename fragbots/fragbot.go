304f7fa2f90f52a7dab0399e6258e4b69a03fa94
Ishaan Rao <ishaanrao@ishaans-mbp.lan>
2022-08-06 15:01:38 -0700

    Added missing space in embed

--- fragbots/fragbot.go
+++ fragbots/fragbot.go
@@ -187,7 +187,7 @@ func onParty(ign string) {
 	_, err = logWebhook.CreateMessage(discord.NewWebhookMessageCreateBuilder().
 		SetEmbeds(discord.NewEmbedBuilder().
 			SetTitle(botName+" Logs").
-			SetDescription(ign+"just partied"+botName+"!\nQueue Position: "+strconv.FormatInt(int64(queueLen), 10)+"\nEstimated Time: `"+strconv.FormatInt(int64((queueLen-1)*5), 10)+" seconds"+"`").
+			SetDescription(ign+" just partied "+botName+"!\nQueue Position: "+strconv.FormatInt(int64(queueLen), 10)+"\nEstimated Time: `"+strconv.FormatInt(int64((queueLen-1)*5), 10)+" seconds"+"`").
 			SetThumbnail("https://mc-heads.net/avatar/"+ign).
 			SetTimestamp(time.Now()).
 			SetColor(DefaultEmbedColor).
